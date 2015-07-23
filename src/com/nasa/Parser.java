e03d0d9afe6055edd00daaf02f700354e82f5901
varshapalat <varshapalat@gmail.com>
2015-07-23 23:22:07 +0530

    Varsha|#0002|Added the build.xml file

---
+++ src/com/nasa/Parser.java
@@ -0,0 +1,4 @@
+package com.nasa;
+
+public class Parser {
+}
---
+++ test/com/nasa/ParserTest.java
@@ -0,0 +1,13 @@
+package com.nasa;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ParserTest {
+
+    @Test
+    public void testingXML(){
+        assertEquals(0,0);
+    }
+}
