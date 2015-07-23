d7f436cd2aa0ebe6989a57ea8172a592598a58ef
varshapalat <varshapalat@gmail.com>
2015-07-24 00:49:09 +0530

    Varsha|#0004|Introduced complete functionality of RoboticRover

--- src/com/nasa/RoboticRover.java
+++ src/com/nasa/RoboticRover.java
@@ -5,17 +5,60 @@ public class RoboticRover {
     int yPosition;
     char facingDirection;
     String letters;
-    public RoboticRover(int initialX, int initialY, char initialDirection, String letters){
+
+    public RoboticRover(int initialX, int initialY, char initialDirection, String letters) {
         this.xPosition = initialX;
         this.yPosition = initialY;
         this.facingDirection = initialDirection;
         this.letters = letters;
     }
 
-    public void changePosition() {
-        xPosition = 1;
-        yPosition = 3;
-        facingDirection = 'N';
+    public void initialProcessing(){
+
+        int length = letters.length();
+        char[] charArrayOfLetters = new char[10];
+        for(int i=0; i<length; i++)
+        {
+            charArrayOfLetters[i] = letters.charAt(i);
+        }
+        for(int i=0; i<length; i++)
+        {
+            changePosition(charArrayOfLetters[i]);
+        }
+    }
+
+    private void changePosition(char next) {
+
+        if (next == 'L') {
+            if (facingDirection == 'N')
+                facingDirection = 'W';
+            else if (facingDirection == 'S')
+                facingDirection = 'E';
+            else if (facingDirection == 'W')
+                facingDirection = 'S';
+            else if (facingDirection == 'E')
+                facingDirection = 'N';
+        }
+        else if (next == 'R') {
+            if (facingDirection == 'N')
+                facingDirection = 'E';
+            else if (facingDirection == 'S')
+                facingDirection = 'W';
+            else if (facingDirection == 'W')
+                facingDirection = 'N';
+            else if (facingDirection == 'E')
+                facingDirection = 'S';
+        }
+        else if (next == 'M') {
+            if (facingDirection == 'N')
+                yPosition++;
+            else if (facingDirection == 'S')
+                yPosition--;
+            else if (facingDirection == 'W')
+                xPosition--;
+            else if (facingDirection == 'E')
+                xPosition++;
+        }
     }
 
     @Override
--- test/com/nasa/RoboticRoverTest.java
+++ test/com/nasa/RoboticRoverTest.java
@@ -12,8 +12,19 @@ public class RoboticRoverTest {
         String letters = "LMLMLMLMM";
         RoboticRover rover = new RoboticRover(1, 2, direction, letters);
 
-        rover.changePosition();
+        rover.initialProcessing();
 
         assertEquals("1 3 N", rover.toString());
+    }
+
+    @Test
+    public void testRoverWithInitialPositonThreeThreeEast() {
+        char direction = 'E';
+        String letters = "MMRMMRMRRM";
+        RoboticRover rover = new RoboticRover(3, 3, direction, letters);
+
+        rover.initialProcessing();
+
+        assertEquals("5 1 E", rover.toString());
     }
 }
